bdaa43ac085ed5a0c421a7473340209ce277a0f2
Dong Zheng <nicole.zheng.nj@hotmail.com>
2013-07-23 15:35:06 +0800

    etude 7-3

--- stats.erl
+++ stats.erl
@@ -2,6 +2,9 @@
 -export([minimum/1]).
 -export([maximum/1]).
 -export([range/1]).
+-export([mean/1]).
+-export([stdv/1]).
+-export([sum/1, sum_of_squares/1]).
 
 minimum(List) -> 
   [H|_] = List,
@@ -30,3 +33,17 @@ maximum([H|T], C) ->
 range(List) ->
   [H|_] = List,
   [minimum(List, H)|[maximum(List, H)]].
+
+sum(List) ->
+  lists:foldl(fun(X, Y) -> X + Y end, 0, List).
+
+mean(List) -> 
+  sum(List)/length(List).
+
+sum_of_squares(List) ->
+  lists:foldl(fun(X, Y) -> X * X + Y end, 0, List).
+
+stdv(List) -> 
+  N = length(List),
+  Sum = sum(List),
+  math:sqrt((sum_of_squares(List) * N - Sum * Sum) / (N * (N - 1))).
