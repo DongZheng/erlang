103b7b3b637ad58d6bb5404ce24d99827a181504
Dong Zheng <nicole.zheng.nj@hotmail.com>
2013-08-13 11:02:15 +0800

    etude 5-1: rewrite

--- ask_area.erl
+++ ask_area.erl
@@ -1,38 +1,71 @@
 -module(ask_area).
--export([area/0]).
+-export([area/0, get_dimensions/2]).
 -import(geom, [area/3]).
 
 area() -> 
-  C = io:get_chars("R)ectangle, T)riangle, or E)llipse >", 1),
+  % Don't use get_chars/2 here, otherwise the proceeding get_line/1 will report error
+  I = io:get_line("R)ectangle, T)riangle, or E)llipse >"),
+  C = hd(I),
   S = char_to_shape(C),
-  io:format("S=~w~n", [S]),
   Numbers = case S of 
-  	unknown -> {"error", "Unknown shape " ++ C};
+    % you can't write '"Unknown shape " ++ C' since C is a char, not a string
+  	unknown -> {error, "Unknown shape " ++ I};
   	rectangle -> get_dimensions("width", "height");
   	triangle -> get_dimensions("base", "height");
     ellipse -> get_dimensions("major axis", "minor axis")
   end,
+  % io:format("Numbers = ~p~n", [Numbers]),
   calculate(S, element(1, Numbers), element(2, Numbers)).
 
+% Given a character parameter, return an atom representing the specified shape
 char_to_shape(C) ->
-  case string:to_lower(C) of
-  	"r" -> rectangle;
-    "t" -> triangle;
-    "e" -> ellipse;
+  case C of
+  	$R -> rectangle;
+    $r -> rectangle;
+    $T -> triangle;
+    $t -> triangle;
+    $E -> ellipse;
+    $e -> ellipse;
     _ -> unknown
   end.
 
+% Returns the number that was input, accept either integers or floats.
 get_number(Prompt) ->
   Str = io:get_line("Enter " ++ Prompt ++ " > "),
-  % Value = string:strip(Input, right, $\n),
-  {N, _} = string:to_integer(Str),
-  N.
+  % string:to_float requires a decimal point; if you just enter input like "3", 
+  % you will receive {error, no_float} for your efforts.
+  % You should try to convert to float first, 
+  {N, _} = string:to_float(Str),
+  case N of 
+    % if that fails, try a conversion to integer
+    error ->
+      {M, _} = string:to_integer(Str),
+      case M of 
+        % if still fails, return error atom
+        error -> error;
+        _ -> M
+      end;
+    _ -> N
+  end.
 
+% Takes two prompts as its parameters (one for each dimension), calls get_number/1 twice.
+% Returns a tuple {N1, N2} with the dimensions.
 get_dimensions(P1, P2) -> 
-  io:format("dimension1=" ++ P1 ++ ", dimension2=" ++ P2 ++ "~n"),
   D1 = get_number(P1),
+  % io:format(P1 ++ "=~w~n", [D1]),
   D2 = get_number(P2),
+  % io:format(P2 ++ "=~w~n", [D2]),
   {D1, D2}.
 
+% Take a shape (as an atom) and two dimensions as its parameters.
+% If the shape is unknown, displays error msg
 calculate(unknown, _, Err) -> io:format("~s~n", [Err]);
-calculate(S, N1, N2) -> geom:area(S, N1, N2).
+% If either dimension is not numeric, or is negative numeric, displays error msg
+calculate(_, error, _) -> io:format("Error in first number~n");
+calculate(_, _, error) -> io:format("Error in second number~n");
+% Calculate the area of the shape
+calculate(S, N1, N2) -> 
+  if 
+    N1 < 0; N2 < 0 -> io:format("Both numbers must be greater than or equal to zero.~n");
+    true -> geom:area(S, N1, N2)
+  end.
